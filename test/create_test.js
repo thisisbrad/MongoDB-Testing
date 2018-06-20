20c08aea14c9ca99807283b7a42cd1fa1ef00bf9
Brad Beltowski <bradbeltowski@ASUS-Hackintosh.local>
2018-06-20 18:56:08 -0400

    testing user model

---
+++ test/create_test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const User = require('../src/models/User');
+
+describe('Creating records', () => {
+  it('saves a user', () => {
+    // create new user
+    // save it to DB
+    // make sure it was successful
+  });
+});
