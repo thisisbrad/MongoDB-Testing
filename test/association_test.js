7200ba2d7ac6dfd07921382e602f9bc1d26cdbf4
Brad Beltowski <bradbeltowski@ASUS-Hackintosh.local>
2018-06-21 21:51:35 -0400

    full relation tree testing in Mongoose and Mocha

--- test/association_test.js
+++ test/association_test.js
@@ -26,12 +26,40 @@ describe('associations', () => {
     Promise.all(saveRequests).then(() => done());
   });
 
-  it.only('save a relationship between a User and a BlogPost', done => {
+  it('saves a relationship between a User and a BlogPost', done => {
     // check for BlogPost._id in User.blogPosts
     User.findOne({ name: 'Bob' })
       .populate('blogPosts')
       .then(user => {
         assert(user.blogPosts[0].title === 'Best Burgers');
+        done();
+      });
+  });
+
+  it.only('save a full relation graph bewteen User, BlogPost, Comment', done => {
+    //
+    User.findOne({ name: 'Bob' })
+      .populate({
+        path: 'blogPosts',
+        populate: {
+          path: 'comments',
+          model: 'comment',
+          populate: {
+            path: 'user',
+            model: 'user'
+          }
+        }
+      })
+      .then(user => {
+        //
+        // console.log('???', user.blogPosts[0].comments[0]);
+        assert(user.name === 'Bob');
+        assert(user.blogPosts[0].title === 'Best Burgers');
+        assert(
+          user.blogPosts[0].comments[0].content ===
+            'What is the burger of the day?'
+        );
+        assert(user.blogPosts[0].comments[0].user.name === 'Bob');
         done();
       });
   });
